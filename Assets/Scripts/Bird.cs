605e0d2e14be5a8822130cbbc865b0bfd94c143a
OukaMiyuki <asukaloid.blog@gmail.com>
2020-09-02 23:31:07 +0700

    First Commit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Bird : MonoBehaviour {
+    private void OnMouseDown() {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    private void OnMouseUp() {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
